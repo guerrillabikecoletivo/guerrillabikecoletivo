dd6d59b7d6e1defeb0a2d77569e34c32af712c5e
luisamtavares <luisatavares.m@gmail.com>
2019-11-24 18:32:24 -0300

    Updates

---
+++ precache-manifest.b455c3ec87fc68ba6973df5a16a4b1ab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134d61f1ccb4d19b8aca68aa1b93a67e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6723c8b7d5832bccbefc",
+    "url": "/static/css/main.41accd0a.chunk.css"
+  },
+  {
+    "revision": "8b1431bca45cc73d3356",
+    "url": "/static/js/2.57bcfe0d.chunk.js"
+  },
+  {
+    "revision": "6723c8b7d5832bccbefc",
+    "url": "/static/js/main.33fbdf66.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c03ecca2c74c445790f0ef58b902ae75",
+    "url": "/static/media/foto1.c03ecca2.jpg"
+  },
+  {
+    "revision": "82e41305c8d340fed7b914f26fafe8ec",
+    "url": "/static/media/foto2.82e41305.jpg"
+  },
+  {
+    "revision": "35dd959018a2aca5508e4c09c61827c3",
+    "url": "/static/media/foto3.35dd9590.jpg"
+  },
+  {
+    "revision": "97b9c071e5574ab5e128ed596e18bfeb",
+    "url": "/static/media/foto4.97b9c071.jpg"
+  },
+  {
+    "revision": "3f1611af01ecb3b8c8d4bdb770b84598",
+    "url": "/static/media/foto5.3f1611af.jpg"
+  },
+  {
+    "revision": "dae98846341e097f72ee1ab04f2b2960",
+    "url": "/static/media/foto6.dae98846.jpg"
+  },
+  {
+    "revision": "f49a4a53a830121b72059746ff2f67b7",
+    "url": "/static/media/foto7.f49a4a53.jpg"
+  },
+  {
+    "revision": "1ad8268cb58d8f3c6fdc03f48018cd77",
+    "url": "/static/media/foto8.1ad8268c.jpg"
+  },
+  {
+    "revision": "07c756aade0f686c3aca4d8d76db80d8",
+    "url": "/static/media/grupo.07c756aa.jpg"
+  },
+  {
+    "revision": "b59cae6377d643d350cdc0f6a12803b6",
+    "url": "/static/media/logocrop.b59cae63.jpg"
+  }
+]);
