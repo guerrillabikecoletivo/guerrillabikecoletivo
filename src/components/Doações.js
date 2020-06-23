ae1a76fe084d4fdfa48aa385804a9e7d6e6ca5a9
Luisa <luisatavares.m@gmail.com>
2020-06-23 17:38:07 -0300

    changed page height in Doações

--- src/components/Doações.js
+++ src/components/Doações.js
@@ -3,7 +3,7 @@ import oficina from '../images/oficina.jpg';
 
 const Doações = () => {
   return (
-    <div className="form" >
+    <div>
       <h1>Doações</h1>
       <p>É muito comum bicicletas ficarem abandonadas na garagem das casas ou nos bicicletários de condomínios (onde às vezes existem bikes empilhadas enferrujando, abandonadas por antigos moradores). O Guerrilla existe para, através de oficinas comunitárias, recuperar essas bicis e doar elas a quem precisa, ao mesmo tempo em que ensinamos pessoas a fazer a manutenção de bicicletas e se tornar mais autônomas.</p>
       <p>Se você tem aquela bicicleta parada há anos na garagem, até queria que outra pessoa pudesse usar, mas não sabe arrumar ela nem pra quem destinar, fala com a gente!</p>
